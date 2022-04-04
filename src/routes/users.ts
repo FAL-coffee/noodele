74fb656916aea404f954f73c2482fdc46c98a5c3
neighbor.net <neighbor.net.official@gmail.com>
2022-04-04 14:41:07 +0000

    fix:res.send to res.status.send

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,11 @@
-import  { Response } from 'express'
+import { Request, Response } from "express";
 // var router = express.Router();
 
 // export default router.get('/', (res:Response) => {
 //   res.send('respond with a resource');
 // });
 
-
 /* GET users listing. */
-export const index = ( res: Response ) => {
-  res.send('respond with a resource');
+export const index = (_: Request, res: Response) => {
+  res.status(200).send("respond with a resource");
 };
