ddb5903c56f5b57462c291263cad0d2836eb6a99
FAL-coffee <fal.engineer.2001@gmail.com>
2022-04-18 14:09:50 +0000

    add:sendMail service called

--- src/controllers/account.ts
+++ src/controllers/account.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { sendRegistorationMail } from "../services/account";
 
 export const index = async (_: Request, res: Response) => {
   res.render("account");
 };
 
 export const create = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  console.log(email, password);
-  res.redirect("new");
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(422).json({ errors: errors.array() });
+    }
+    await sendRegistorationMail(req);
+    res.status(200).json({ message: "send complate" });
+  } catch (error) {
+    throw error;
+  }
 };
