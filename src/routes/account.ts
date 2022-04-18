3a5c3651e6d6a3222b165b5d0a03ea98670fe32d
FAL-coffee <fal.engineer.2001@gmail.com>
2022-04-18 14:09:19 +0000

    add:validations for post new controller orgs

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import * as accountController from "../controllers/account";
+import { validations } from "../services/account";
 const router = express.Router({ mergeParams: true });
 
 router.get("/new", accountController.index);
-router.post("/new", accountController.create);
+router.post("/new", validations, accountController.create);
 
 export default router;
