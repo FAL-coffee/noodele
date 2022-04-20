47d4a54c99d80be6ef0a146495c878a774aa73f8
neighbor.net <neighbor.net.official@gmail.com>
2022-04-20 06:37:46 +0000

    add:removeUserById service

---
+++ src/services/users.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export const getUserList = async () => {
+  try {
+    const users = await prisma.user.findMany();
+    return users;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const removeUserById = async (userId: string) => {
+  try {
+    await prisma.user.delete({ where: { id: userId } });
+  } catch (error) {
+    throw error;
+  }
+};
