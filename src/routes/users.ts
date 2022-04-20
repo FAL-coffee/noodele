94045f9d45e957947d3ca277fdf622e4d9242dbc
neighbor.net <neighbor.net.official@gmail.com>
2022-04-20 06:22:15 +0000

    add:users routes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,37 +1,7 @@
-import { Request, Response } from "express";
-// var router = express.Router();
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-// export default router.get('/', (res:Response) => {
-//   res.send('respond with a resource');
-// });
+import express from "express";
+import * as userController from "../controllers/users";
+const router = express.Router({ mergeParams: true });
 
-/* GET users listing. */
-export const index = async (_: Request, res: Response) => {
-  const users = await prisma.user.findMany();
-  res.render("users", { title: "users", items: users });
-};
+router.get("/", userController.index);
 
-export const create = async (req: Request, res: Response) => {
-  // console.log(req.body);
-  const { name, password, email } = req.body;
-  await prisma.user.create({ data: { name, password, email } });
-  // const users = await prisma.user.findMany();
-  // res.render("index", { title: "home", items: users });
-  res.redirect("/users");
-};
-
-export const remove = async (req: Request, res: Response) => {
-  await prisma.user.delete({ where: { id: req.params?.id } });
-  res.redirect("/users");
-};
-
-export const update = async (req: Request, res: Response) => {
-  console.log(req.body);
-  const { name, password } = req.body;
-  await prisma.user.update({
-    where: { id: req.params?.id },
-    data: { name, password },
-  });
-  res.redirect("/users");
-};
+export default router;
