108c0370466a549150b3b217846b4a5c75e279cc
neighbor.net <neighbor.net.official@gmail.com>
2022-04-20 06:48:26 +0000

    add:update routing

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,5 +3,7 @@ import * as userController from "../controllers/users";
 const router = express.Router({ mergeParams: true });
 
 router.get("/", userController.index);
+router.post("/delete/:id", userController.index);
+router.post("/update/:id", userController.index);
 
 export default router;
