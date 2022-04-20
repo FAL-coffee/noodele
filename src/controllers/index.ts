3a7eda1c2890561f1e5174118fad29d1189444d0
neighbor.net <neighbor.net.official@gmail.com>
2022-04-20 06:22:48 +0000

    update:toppage render

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from "express";
-import * as services from "../services/index";
 
 export const index = async (_: Request, res: Response) => {
-  const users = await services.getUserList();
-  res.render("users", { title: "users", items: users });
+  res.render("index");
 };
