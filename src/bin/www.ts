aef668de9563de61b00b9910c2eb4e8eaac1ceb9
neighbor.net <neighbor.net.official@gmail.com>
2022-04-21 18:26:49 +0900

    update:title -> noodele:server

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -1,14 +1,14 @@
-import app from '../app';
-import * as http from 'http';
-import * as debugModule from 'debug';
-var debug = debugModule.debug('quick-start-express-typescript:server');
+import app from "../app";
+import * as http from "http";
+import * as debugModule from "debug";
+var debug = debugModule.debug("noodele:server");
 
 /**
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
 
 /**
  * Create HTTP server.
@@ -21,8 +21,8 @@ const server = http.createServer(app);
  */
 
 server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+server.on("error", onError);
+server.on("listening", onListening);
 
 /**
  * Normalize a port into a number, string, or false.
@@ -49,21 +49,19 @@ function normalizePort(val: string): number | string | boolean {
  */
 
 function onError(error: any): void {
-  if (error.syscall !== 'listen') {
+  if (error.syscall !== "listen") {
     throw error;
   }
 
-  const bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
       process.exit(1);
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
       process.exit(1);
     default:
       throw error;
@@ -75,20 +73,20 @@ function onError(error: any): void {
  */
 
 function onListening(): void {
-  function bind():string|void {
+  function bind(): string | void {
     const addr = server.address();
     if (addr === null) {
-      return '';
+      return "";
     }
 
-    if (typeof addr === 'string') {
-      return 'pipe ' + addr;
+    if (typeof addr === "string") {
+      return "pipe " + addr;
     }
 
-    if ('port' in addr) {
-      return 'port ' + addr.port;
+    if ("port" in addr) {
+      return "port " + addr.port;
     }
   }
 
-  debug('Listening on ' + bind());
+  debug("Listening on " + bind());
 }
