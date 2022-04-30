9d32d3254a2564cc599a1a43dab5017e9d261309
neighbor.net <neighbor.net.official@gmail.com>
2022-04-30 15:49:20 +0000

    update:index routes return view

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
-import express from "express";
-import * as indexController from "../controllers/index";
+import express, { Request, Response } from "express";
 import userRouter from "./users";
 import accountRouter from "./account";
 import authRouter from "./auth";
 
 const router = express.Router();
 
-router.get("/", indexController.index);
+router.get("/", (_: Request, res: Response) => {
+  res.render("index");
+});
+
 router.use("/users", userRouter);
 router.use("/account", accountRouter);
 router.use("/auth", authRouter);
